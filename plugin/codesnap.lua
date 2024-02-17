4941857970a89fcfe662a527925554b7ac35eb6d
Mist <mist.zzh@gmail.com>
2024-02-17 23:39:39 +0800

    [Feat] remove rust lib plugin

--- plugin/codesnap.lua
+++ plugin/codesnap.lua
@@ -1,6 +1,5 @@
 local codesnap = require("codesnap")
-local foo = require("foo")
 
-foo.say_hello("Mist")
-
-vim.api.nvim_create_user_command("CodeSnap", function() end, {})
+vim.api.nvim_create_user_command("CodeSnap", function()
+  codesnap.setup()
+end, {})
