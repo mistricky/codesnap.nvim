a11ac2246d3064c23ee2187a5cfaeba5b9e3974b
Mist <mist.zzh@gmail.com>
2024-02-24 18:07:41 +0800

    [Feat] distinguish dev and prod in server and client (#19)

--- snap-server/src/port.rs
+++ snap-server/src/port.rs
@@ -1,6 +1,14 @@
 use std::net::TcpListener;
 
 pub fn get_available_port() -> Option<u16> {
+    if cfg!(debug_assertions) {
+        Some(8080)
+    } else {
+        find_available_port()
+    }
+}
+
+fn find_available_port() -> Option<u16> {
     (8000..10000).find(|port| port_is_available(*port))
 }
 
