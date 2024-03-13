4fe7e20da810da70f024539ead99aae941f87cbd
Mist <mist.zzh@gmail.com>
2024-03-13 16:53:32 +0800

    [Feat] compile nvim-0.9 FFI

--- generator/src/save.rs
+++ generator/src/save.rs
@@ -4,11 +4,7 @@ use nvim_oxi::{print, Result};
 pub fn save_snapshot(config: TakeSnapshotParams) -> Result<()> {
     match &config.save_path {
         Some(path) => {
-            take_snapshot(config.clone())
-                .save_png(path)
-                .unwrap_or_else(|path| {
-                    print!("Save {} failed!", path);
-                });
+            let _ = take_snapshot(config.clone()).save_png(path);
         }
         None => {
             print!("Cannot find 'save_path' in config");
