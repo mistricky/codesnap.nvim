61e9f636b847296525623d007c1ccdc7535908ec
Mist <mist.zzh@gmail.com>
2024-03-13 15:15:36 +0800

    [Chore] remove unused code

--- lua/codesnap/static.lua
+++ lua/codesnap/static.lua
@@ -1,15 +1,5 @@
 local path_utils = require("codesnap.utils.path")
 
--- pub mac_window_bar: bool,
---     pub watermark: Option<String>,
---     pub title: Option<String>,
---     pub code_font_family: String,
---     pub watermark_font_family: String,
---     pub code: String,
---     pub language: Option<String>,
---     pub extension: Option<String>,
---     pub save_path: Option<String>,
-
 return {
   config = {
     mac_window_bar = true,
@@ -17,7 +7,6 @@ return {
     title = "CodeSnap.nvim",
     code_font_family = "CaskaydiaCove Nerd Font",
     watermark_font_family = "Pacifico",
-    save_path = "/Users/zhanhaozhao/repositories/codesnap.nvim/asd.png",
   },
   cwd = path_utils.back(path_utils.back(debug.getinfo(1, "S").source:sub(2):match("(.*[/\\])"))),
   preview_switch = true,
