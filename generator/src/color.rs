33329133afe72447318a01b4cc68bf95555a53cc
Mist <mist.zzh@gmail.com>
2024-03-31 18:13:36 +0800

    [Feat] convert rgb hex color to rgba hex color

--- generator/src/color.rs
+++ generator/src/color.rs
@@ -1,27 +1,44 @@
 use std::i64;
 use tiny_skia::Color;
 
+const HEX_COLOR_LENGTH: usize = 7;
+const HEX_COLOR_WITH_ALPHA_LENGTH: usize = 9;
+
 pub struct RgbaColor {
     pub color: Color,
 }
 
+pub fn is_valid_hex_color(color: &str) -> bool {
+    (color.len() == HEX_COLOR_LENGTH || color.len() == HEX_COLOR_WITH_ALPHA_LENGTH)
+        && color.starts_with("#")
+}
+
+fn parse_color_to_rgba_hex(hex: &str) -> String {
+    if !is_valid_hex_color(&hex) || hex.len() == HEX_COLOR_WITH_ALPHA_LENGTH {
+        hex.to_string()
+    } else {
+        format!("{}ff", hex)
+    }
+}
+
 impl Into<RgbaColor> for String {
     fn into(self) -> RgbaColor {
-        let hex_color = &self.to_lowercase()[1..self.len()];
+        let rgba_hex_color = parse_color_to_rgba_hex(&self);
+        // Remove the '#' symbol
+        let hex_color = &rgba_hex_color.to_lowercase()[1..rgba_hex_color.len()];
         let chars = hex_color.chars().collect::<Vec<char>>();
         let splits = &chars
             .chunks(2)
             .map(|chunk| i64::from_str_radix(&chunk.iter().collect::<String>(), 16).unwrap())
             .collect::<Vec<_>>();
-        
-        let alpha: i64;
-        match splits.get(3) {
-            Some(x) => alpha = *x,
-            None => alpha = 255,
-        }
 
         RgbaColor {
-            color: Color::from_rgba8(splits[0] as u8, splits[1] as u8, splits[2] as u8, alpha as u8),
+            color: Color::from_rgba8(
+                splits[0] as u8,
+                splits[1] as u8,
+                splits[2] as u8,
+                splits[3] as u8,
+            ),
         }
     }
 }
