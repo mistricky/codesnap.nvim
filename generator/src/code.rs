69ecdace3bc17f4b9b731cad0969a596ef74748f
The Mist <mist.zzh@gmail.com>
2024-04-07 16:17:24 +0800

    [Fix] replace all tab character with space for correct render text (#77)

--- generator/src/code.rs
+++ generator/src/code.rs
@@ -11,7 +11,7 @@ fn min_width(width: f32) -> f32 {
 }
 
 pub fn calc_wh(text: &str, char_wdith: f32, line_height: f32) -> (f32, f32) {
-    let trimmed_text = trim_space(text);
+    let trimmed_text = prepare_code(text);
     let lines = trimmed_text.lines();
     let max_length_line = lines.clone().into_iter().fold("", |max_length_line, cur| {
         if cur.len() > max_length_line.len() {
@@ -26,7 +26,16 @@ pub fn calc_wh(text: &str, char_wdith: f32, line_height: f32) -> (f32, f32) {
     (min_width(width), height)
 }
 
-pub fn trim_space(text: &str) -> String {
+// The tab character is incorrectly render using cosmic, need to replace all tab with space
+// before render the code
+fn replace_tab_to_space(text: &str) -> String {
+    let spaces = " ".repeat(2);
+
+    str::replace(text, "\t", &spaces)
+}
+
+// If the first line have indention, remove the same indention from subsequent lines
+fn trim_space(text: &str) -> String {
     let lines = text.split("\n").collect::<Vec<&str>>();
     let first_line = lines.first().unwrap();
     let head_spaces = Regex::new(r"^(\s*)").unwrap().find(first_line);
@@ -46,4 +55,8 @@ pub fn trim_space(text: &str) -> String {
         }
         None => text.to_string(),
     }
+}
+
+pub fn prepare_code(code: &str) -> String {
+    replace_tab_to_space(&trim_space(&code))
 }
