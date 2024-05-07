daf3dc6ee3a5c8545d2196d4f44f0a7c5086ce49
Mist <mist.zzh@gmail.com>
2024-05-07 21:34:14 +0800

    [Feat] add show_workspace config for display workspace in breadcrumbs

--- lua/codesnap/utils/path.lua
+++ lua/codesnap/utils/path.lua
@@ -1,17 +1,29 @@
 local string_utils = require("codesnap.utils.string")
 local path_utils = {}
 
+function path_utils.get_escaped_cwd()
+  local cwd = vim.fn.getcwd()
+
+  return string_utils.escape(cwd)
+end
+
 function path_utils.back(path)
   local parsed_path, _ = path:gsub("/[^\\/]+/?$", "")
 
   return parsed_path
 end
 
+function path_utils.get_workspace()
+  local cwd = vim.fn.getcwd()
+  local _, _, workspace = string.find(cwd, "/([^/]+)$")
+
+  return workspace == nil and "" or workspace
+end
+
 function path_utils.get_relative_path()
   local full_file_path = vim.fn.expand("%:p")
-  local cwd = vim.fn.getcwd()
 
-  return full_file_path:gsub(string_utils.escape(cwd), ""):sub(2)
+  return full_file_path:gsub(path_utils.get_escaped_cwd(), ""):sub(2)
 end
 
 return path_utils
