c5ac5374ff61830d80408d37ca4a2f2f4d8520c1
The Mist <mist.zzh@gmail.com>
2025-10-15 21:12:04 +0800

    [Feat] create highlight functions

--- lua/codesnap/config.lua
+++ lua/codesnap/config.lua
@@ -3,36 +3,49 @@ local path_utils = require("codesnap.utils.path")
 local string_utils = require("codesnap.utils.string")
 local static = require("codesnap.static")
 local table_utils = require("codesnap.utils.table")
+local module = require("codesnap.module")
 local config_module = {}
 
 local function get_file_path(show_workspace)
-	local relative_path = path_utils.get_relative_path()
+  local relative_path = path_utils.get_relative_path()
 
-	return show_workspace and path_utils.get_workspace() .. "/" .. relative_path or relative_path
+  return show_workspace and path_utils.get_workspace() .. "/" .. relative_path or relative_path
 end
 
 function config_module.get_config()
-	local code = visual_utils.get_selected_text()
-	local start_line_number = visual_utils.get_start_line_number()
+  local code = visual_utils.get_selected_text()
+  local start_line_number = visual_utils.get_start_line_number()
 
-	if string_utils.is_str_empty(code) then
-		error("No code is selected", 0)
-		return
-	end
+  if string_utils.is_str_empty(code) then
+    error("No code is selected", 0)
+    return
+  end
 
-	local code_content = {
-		content = code,
-		start_line_number = start_line_number,
-		file_path = get_file_path(static.config.show_workspace),
-	}
+  local code_content = {
+    content = code,
+    start_line_number = start_line_number,
+    highlight_lines = {
+      { 1, 2, static.config.diff_config.add_color },
+      {
+        3,
+        static.config.diff_config.delete_color,
+      },
+      {
+        4,
+        static.config.highlight_color,
+      },
+    },
+    file_path = get_file_path(static.config.show_workspace),
+  }
 
-	local config = table_utils.assign(static.config.snapshot_config, {
-		content = code_content,
-		-- start_line_number = static.config.has_line_number and start_line_number or nil,
-	})
+  local config = table_utils.assign(static.config.snapshot_config, {
+    content = code_content,
+    theme = module.load_generator(true).parse_code_theme(static.config.snapshot_config.theme),
+    -- start_line_number = static.config.has_line_number and start_line_number or nil,
+  })
 
-	-- config.save_path = parse_save_path(config.save_path)
-	return config
+  -- config.save_path = parse_save_path(config.save_path)
+  return config
 end
 
 return config_module
